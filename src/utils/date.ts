836078c3c5f8fbe2a77f85a7f8467eaf41845ba0
Valery Zinchenko <minicablestone@gmail.com>
2022-03-17 08:04:49 +0300

    add utils

---
+++ src/utils/date.ts
@@ -0,0 +1,4 @@
+export function humanizeDate(lang: string, date: Date) {
+  const localeString = date.toLocaleString(lang, { timeStyle: "short", dateStyle: "short" })
+  return localeString.replace(", ", " в ")
+}
