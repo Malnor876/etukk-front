d88737ce50c3943da10c4ef5990685fbf50a62f9
FrameMuse <minicablestone@gmail.com>
2022-03-26 14:26:08 +0300

    patch FavouritesView

--- src/app/views/favourites/index.tsx
+++ src/app/views/favourites/index.tsx
@@ -1,23 +1,21 @@
-import SellerPreview from "app/components/business/SellerPreview/SellerPreview"
 import ButtonLink from "app/components/UI/Button/ButtonLink"
 import SortingToggle from "app/components/UI/SortingToggle/SortingToggle"
 import Switcher from "app/components/UI/Switcher/Switcher"
 import Buttons from "app/layouts/Buttons/Buttons"
 import Previews from "app/layouts/Previews/Previews"
-import LotPreview from "app/views/lot/LotPreview/LotPreview"
+import { IMAGE_MOCKS } from "constants/mocks"
+import LotPreview from "domain/Lot/LotPreview/LotPreview"
+import SellerPreview from "domain/seller/SellerPreview/SellerPreview"
 import { Outlet, Route, Routes } from "react-router"
 import { NavLink } from "react-router-dom"
-
-import mockPNG from "./mock.png"
-import mock2PNG from "./mock-2.png"
 
 function FavouritesView() {
   return (
     <>
       <h2 className="heading">избранное</h2>
       <Buttons>
-        <ButtonLink small outline to="lots">Лоты</ButtonLink>
-        <ButtonLink small outline to="sellers">Продавцы</ButtonLink>
+        <ButtonLink small outline nav to="lots">Лоты</ButtonLink>
+        <ButtonLink small outline nav to="sellers">Продавцы</ButtonLink>
       </Buttons>
       <Routes>
         <Route path="lots/*" element={
@@ -36,11 +34,13 @@ function FavouritesView() {
             <Previews>
               {[...Array(16)].map((_, i) => (
                 <LotPreview
+                  id={i}
                   city="Москва"
                   title="ЗАГОЛОВОК ВМЕСТИТСЯ 2 СТРОКИ НЕ БОЛЬШЕ ЗАГОЛОВОК ВМЕСТИТСЯ 2 СТРОКИ НЕ БОЛЬШЕ"
-                  image={mockPNG}
-                  startBid={100}
-                  startedAt={new Date}
+                  image={IMAGE_MOCKS[0]}
+                  price={100}
+                  tradeStart={new Date}
+                  bookmarked
                   key={i}
                 />
               ))}
@@ -51,7 +51,7 @@ function FavouritesView() {
           <Previews>
             {[...Array(16)].map((_, i) => (
               <SellerPreview
-                avatar={mock2PNG}
+                avatar={IMAGE_MOCKS[1]}
                 name="ИП ПОВЕЛИТЕЛЬ МЕБЕЛИ и мира в целом"
                 city="Москва"
                 likes={5}
