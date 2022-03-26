0e4c8699d1f7f07b06a37868bc8be5ff2c1a022f
FrameMuse <minicablestone@gmail.com>
2022-03-26 14:32:02 +0300

    patch AppRouter

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -5,6 +5,9 @@ import ErrorView from "./views/error"
 import FavouritesView from "./views/favourites"
 import HomeView from "./views/home"
 import LotView from "./views/lot"
+import LotNewView from "./views/lot-new"
+import NotificationsView from "./views/notifications"
+import SupportView from "./views/support"
 
 function AppRouter() {
   return (
@@ -12,10 +15,16 @@ function AppRouter() {
       <Route path="/">
         <Route path="*" element={<ErrorView />} />
         <Route element={<ViewLayout />}>
+
           <Route index element={<HomeView />} />
           <Route path="hot" element={<HomeView />} />
-          <Route path="lot/:lotId" element={<LotView />} />
+
+          <Route path="lots/:lotId/*" element={<LotView />} />
+          <Route path="lots/new/*" element={<LotNewView />} />
+
           <Route path="favourites/*" element={<FavouritesView />} />
+          <Route path="notifications/*" element={<NotificationsView />} />
+          <Route path="support/*" element={<SupportView />} />
         </Route>
       </Route>
     </Routes>
