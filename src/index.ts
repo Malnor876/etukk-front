8a6b66731b78f46e89e969668d8b732fab1cc9ed
FrameMuse <minicablestone@gmail.com>
2022-03-26 14:19:36 +0300

    fix root imports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import App from "app/App"
 import { createElement } from "react"
 import ReactDOM from "react-dom"
 
-import("polyfills")
-import("preload")
+require("polyfills")
+require("preload")
 
 ReactDOM.render(createElement(App), document.getElementById("root"))
