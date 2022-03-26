80aa7b7985fc1d57ffa2c85b74c9fac6a673778f
FrameMuse <minicablestone@gmail.com>
2022-03-26 14:31:56 +0300

    patch App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import "app/assets/scss/base.scss"
 
-import ClientAPI from "api/client"
+import ClientAPI from "infrastructure/persistence/api/client"
+import store from "infrastructure/persistence/redux/store"
 import { ModalContainer } from "modules/modal/container"
 import { ReactNode, Suspense } from "react"
 import { ClientContextProvider } from "react-fetching-library"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
-import store from "redux/store"
 
 import AppRouter from "./AppRouter"
 import CookiesNotice from "./components/containers/CookiesNotice/CookiesNotice"
