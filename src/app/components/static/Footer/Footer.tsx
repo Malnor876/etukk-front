d06481996270a4a201cf702dbde3ec22619c9519
FrameMuse <minicablestone@gmail.com>
2022-03-26 14:28:57 +0300

    patch Footer

--- src/app/components/static/Footer/Footer.tsx
+++ src/app/components/static/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import "./Footer.scss"
 
-import OuterLink from "app/components/OuterLink"
+import OuterLink from "app/components/services/OuterLink"
 import { Link } from "react-router-dom"
 
 function Footer() {
@@ -10,17 +10,17 @@ function Footer() {
         <img src="/static/images/logo.svg" alt="etukk logo" />
       </Link>
       <div className="footer__group">
-        <Link className="footer__link" to="/">Избранное</Link>
-        <Link className="footer__link" to="/">Выставить лот</Link>
-        <small>Все права защищены, 2021©</small>
+        <Link className="footer__link" to="/favourites/lots">Избранное</Link>
+        <Link className="footer__link" to="/lots/new/edit">Выставить лот</Link>
+        <small>Все права защищены, 2022©</small>
       </div>
       <div className="footer__group">
-        <Link className="footer__link" to="/">Уведомления</Link>
-        <Link className="footer__link" to="/">Профиль</Link>
+        <Link className="footer__link" to="/notifications">Уведомления</Link>
+        <Link className="footer__link" to="/user">Профиль</Link>
       </div>
       <div className="footer__group">
-        <Link className="footer__link" to="/">Техподдержка</Link>
-        <Link className="footer__link" to="/">О нас</Link>
+        <Link className="footer__link" to="/support">Тех. поддержка</Link>
+        <Link className="footer__link" to="/about">О нас</Link>
       </div>
       <div className="footer__group">
         <OuterLink className="footer__link" to="tel:+7 (900) 999-99-99" noTarget>+7 (900) 999-99-99</OuterLink>
