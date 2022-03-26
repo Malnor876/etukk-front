957b2ff438d9619436bc4ef180b0a45f8d780d6c
FrameMuse <minicablestone@gmail.com>
2022-03-26 14:30:51 +0300

    add Picker

---
+++ src/app/components/UI/Picker/Picker.tsx
@@ -0,0 +1,33 @@
+import "./Picker.scss"
+
+import { Children, ComponentProps, Dispatch, ReactElement, useEffect, useState } from "react"
+import { classWithModifiers } from "utils/common"
+
+interface PickerProps<V> {
+  name?: string
+  width?: string
+  defaultValue?: V
+  onChange?: Dispatch<V>
+  children: ReactElement<ComponentProps<"option"> & { value: V }>[]
+}
+
+function Picker<V>(props: PickerProps<V>) {
+  const [options, setOptions] = useState(Children.map(props.children, child => child.props))
+  const [choice, setChoice] = useState(options.findIndex(option => option.value === props.defaultValue))
+  useEffect(() => setOptions(Children.map(props.children, child => child.props)), [props.children])
+  function choose(index: number) {
+    const value = options[index].value as unknown as V
+
+    setChoice(index)
+    props.onChange?.(value)
+  }
+  return (
+    <div className="picker" role="list" aria-details="pick a value">
+      {options.map((option, index) => (
+        <div className={classWithModifiers("picker__entry", choice === index && "active")} aria-selected={choice === index} role="option" onClick={() => choose(index)} key={index}>{option.children}</div>
+      ))}
+    </div>
+  )
+}
+
+export default Picker
