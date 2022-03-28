68496fce864e48b8e7f824f5a502d1e94b767674
FrameMuse <minicablestone@gmail.com>
2022-03-28 16:13:53 +0300

    add mocks

--- src/constants/mocks.ts
+++ src/constants/mocks.ts
@@ -3,3 +3,12 @@ import mock2JPG from "./mock-2.jpg"
 import mock3JPG from "./mock-3.jpg"
 
 export const IMAGE_MOCKS = [mockPNG, mock2JPG, mock3JPG]
+export const LOT_PREVIEW_MOCK = {
+  id: 0,
+  city: "Москва",
+  title: "ЗАГОЛОВОК ВМЕСТИТСЯ 2 СТРОКИ НЕ БОЛЬШЕ ЗАГОЛОВОК ВМЕСТИТСЯ 2 СТРОКИ НЕ БОЛЬШЕ",
+  image: IMAGE_MOCKS[1],
+  price: 100,
+  tradeStart: new Date,
+  bookmarked: true
+}
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const FILE_TYPES = Object.freeze({
-  IMAGE: Object.freeze(["JPEG", "PNG"] as const),
+  IMAGE: Object.freeze(["JPEG", "JPG", "PNG"] as const),
   VIDEO: Object.freeze(["MP4", "M4V", "MOV", "3GP", "3G2", "WMV", "ASF", "AVI", "FLV", "MKV", "WEBM"] as const)
 })
 
