21bc173245820a1d6550a494410d4238eb097b91
FrameMuse <minicablestone@gmail.com>
2022-04-04 19:27:39 +0300

    FullscreenSignIn indent fix

--- src/app/components/modals/auth/FullscreenSignIn.tsx
+++ src/app/components/modals/auth/FullscreenSignIn.tsx
@@ -1,6 +1,7 @@
 import Password from "app/components/containers/Password/Password"
 import Button from "app/components/UI/Button/Button"
 import Input from "app/components/UI/Input/Input"
+import { Column } from "app/layouts/BaseLayouts/BaseLayouts"
 import Form from "app/layouts/Form/Form"
 import FullscreenLayout from "app/layouts/Modal/FullscreenLayout/FullscreenLayout"
 import { Modal } from "modules/modal/controller"
@@ -14,9 +15,11 @@ function FullscreenSignIn() {
     <FullscreenLayout>
       <h3 className="heading" style={{ textAlign: "center" }}>ВХОД</h3>
       <Form centered onChange={event => setValidity(event.currentTarget.checkValidity())}>
-        <Input type="email" name="email" width="20em" placeholder="Е-mail или номер телефона" required />
-        <Password name="password" width="20em" />
-        <Button type="submit" disabled={!validity}>Войти</Button>
+        <Column>
+          <Input type="email" name="email" width="20em" placeholder="Е-mail или номер телефона" required />
+          <Password name="password" width="20em" />
+          <div><Button type="submit" disabled={!validity}>Войти</Button></div>
+        </Column>
       </Form>
       <Button color="white" onClick={() => Modal.replace(FullscreenSignUp)}>Регистрация</Button>
     </FullscreenLayout >
