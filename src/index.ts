b87debb73527a3d71d3ee6d102b9897dd231b2f7
FrameMuse <minicablestone@gmail.com>
2022-04-16 12:22:20 +0300

    paid patch

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-// import App from "app/App"
+import App from "app/App"
 import PreInit from "preinit"
-// import { createElement } from "react"
-// import { createRoot } from "react-dom/client"
+import { createElement } from "react"
+import { createRoot } from "react-dom/client"
 
 require("polyfills")
 
@@ -9,8 +9,8 @@ function init() {
   const rootElement = document.getElementById("root")
   if (rootElement === null) return
 
-  // const root = createRoot(rootElement)
-  // root.render(createElement(App))
+  const root = createRoot(rootElement)
+  root.render(createElement(App))
 }
 
-if (PreInit(true)) init()
+if (PreInit(false)) init()
