b865c98b1684292e8c413d0d60b2107c90900b9d
FrameMuse <minicablestone@gmail.com>
2022-06-10 16:40:35 +0300

    0.9.6f

--- src/infrastructure/persistence/api/mappings/user.ts
+++ src/infrastructure/persistence/api/mappings/user.ts
@@ -5,22 +5,22 @@ import { ExtractActionPayload } from "../client.types"
 import { getUser } from "../data/actions"
 import { mapImageUrl } from "./helpers"
 
-export function mapUser(user: ExtractActionPayload<ReturnType<typeof getUser>>): UserSigned {
-  const [firstName, lastName] = user.fullname.split(" ")
+export function mapUser(user?: ExtractActionPayload<ReturnType<typeof getUser>>): UserSigned {
+  const [firstName, lastName] = (user?.fullname ?? "unknown unknownovich").split(" ")
   return {
     auth: true,
-    id: user.id,
-    avatar: mapImageUrl(user.user_pic?.filename),
-    city: user.city ?? "unknown",
-    fullName: user.fullname,
+    id: user?.id ?? -1,
+    avatar: mapImageUrl(user?.user_pic?.filename),
+    city: user?.city ?? "unknown",
+    fullName: user?.fullname ?? "unknown",
     firstName,
     lastName,
-    type: user.organization ? "organization" : "user",
-    verified: user.verified ?? false,
-    buyerRating: user.seller_rating ?? -1,
-    sellerRating: user.seller_rating ?? -1,
-    email: user.email ?? "unknown",
-    phone: user.phonenumber ?? "unknown"
+    type: user?.organization ? "organization" : "user",
+    verified: user?.verified ?? false,
+    buyerRating: user?.seller_rating ?? -1,
+    sellerRating: user?.seller_rating ?? -1,
+    email: user?.email ?? "unknown",
+    phone: user?.phonenumber ?? "unknown"
   }
 }
 
