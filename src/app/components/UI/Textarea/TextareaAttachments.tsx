2c261f53a8a16dc9fe238e7a02187b35851ab783
FrameMuse <minicablestone@gmail.com>
2022-06-30 07:59:18 +0300

    0.9.13

--- src/app/components/UI/Textarea/TextareaAttachments.tsx
+++ src/app/components/UI/Textarea/TextareaAttachments.tsx
@@ -2,10 +2,10 @@ import "./Textarea.scss"
 
 import { FILE_TYPES } from "consts"
 import _ from "lodash"
-import { DetailedHTMLProps, TextareaHTMLAttributes } from "react"
+import { ChangeEvent, DetailedHTMLProps, TextareaHTMLAttributes, useEffect, useState } from "react"
 import { classMerge } from "utils/common"
 
-import ChooseImage from "../ChooseImage/ChooseImage"
+import CloseButton from "../CloseButton/CloseButton"
 import Icon from "../Icon/Icon"
 
 interface TextareaAttachmentsProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
@@ -13,9 +13,35 @@ interface TextareaAttachmentsProps extends DetailedHTMLProps<TextareaHTMLAttribu
 }
 
 /**
- * When `name` is passed, attachments have its own name with suffix "attachments" -> `[name]-attachments`. 
+ * When `name` is passed, attachment files input will have its own name with suffix "attachments" -> `[name]-attachments`. 
  */
 function TextareaAttachments(props: TextareaAttachmentsProps) {
+  const [files, setFiles] = useState<File[]>([])
+  function addFiles(file: File[]) {
+    if (file.length === 0) {
+      setFiles([])
+      return []
+    }
+
+    const nextFiles = [...files, ...file]
+    setFiles(nextFiles)
+
+    return nextFiles
+  }
+  function removeFile(file: File) {
+    setFiles(files => files.filter(temp => temp !== file))
+  }
+  function onFileInputChange(event: ChangeEvent<HTMLInputElement>) {
+    // https://stackoverflow.com/questions/5632629/how-to-change-a-file-inputs-filelist-programmatically
+
+    const target = event.currentTarget
+
+    const files = addFiles([...target.files ?? []])
+    const dataTransfer = new DataTransfer()
+
+    files.forEach(file => dataTransfer.items.add(file))
+    target.files = dataTransfer.files
+  }
   return (
     <div className="textarea">
       {props.children && (
@@ -24,9 +50,19 @@ function TextareaAttachments(props: TextareaAttachmentsProps) {
       <div className="textarea-attachments" aria-label="textarea with attachments">
         <textarea {..._.omit(props, "children", "width")} style={{ "--textarea-width": props.width }} className={classMerge("textarea__appearance", props.className)} />
         <label className="textarea-attachments__attach" aria-label="attach files" role="button" tabIndex={0}>
-          <input className="textarea-attachments__input" type="file" name={`${props.name}-attachments`} multiple accept={FILE_TYPES.IMAGE.map(type => "." + type).join(",")} />
+          <input className="textarea-attachments__input" type="file" name={`${props.name}-attachments`} multiple accept={FILE_TYPES.IMAGE.map(type => "." + type).join(",")} onChange={onFileInputChange} />
           <Icon name="clip" />
         </label>
+      </div>
+      <div className="textarea-attachments__attachments">
+        {files.map(file => file.type.search(/image|video/) > -1 && (
+          <div className="textarea-attachment">
+            <img src={URL.createObjectURL(file)} alt="attachment" className="textarea-attachment__image" />
+            <div className="textarea-attachment__close">
+              <CloseButton onClick={() => removeFile(file)} />
+            </div>
+          </div>
+        ))}
       </div>
       {/* <ChooseImage /> */}
     </div>
