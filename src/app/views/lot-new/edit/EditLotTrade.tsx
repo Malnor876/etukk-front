a38c6bc8458a521c3bbf2b14eef341ba9ffb8596
FrameMuse <minicablestone@gmail.com>
2022-07-07 16:49:16 +0300

    0.9.19c

--- src/app/views/lot-new/edit/EditLotTrade.tsx
+++ src/app/views/lot-new/edit/EditLotTrade.tsx
@@ -1,5 +1,4 @@
 import Input from "app/components/UI/Input/Input"
-import InputAddress from "app/components/UI/Input/InputAddress"
 import Radio from "app/components/UI/Radio/Radio"
 import Selector from "app/components/UI/Selector/Selector"
 import { Row } from "app/layouts/BaseLayouts/BaseLayouts"
@@ -20,22 +19,34 @@ function EditLotTrade() {
   const [start3] = getBiddingTime(3)
   const [start4] = getBiddingTime(4)
 
+
+  function asd(a: any, f: number) {
+    const b = new Date(a)
+    b.setHours(a.getHours() + f)
+    return b
+  }
+
+  const end1 = asd(start1, 1)
+  const end2 = asd(start2, 2)
+  const end3 = asd(start3, 1)
+  const end4 = asd(start4, 2)
+
   return (
     <section>
       <h2>Торги и доставка</h2>
       <p>Период публикации лота и проведения торгов</p>
-      <Radio name="date" value="1" defaultChecked={date === "1"} onChange={setDate}>Сегодня {humanizeDate(start1)} - {start1.getHours() + 1}:{start1.getMinutes()}</Radio>
+      <Radio name="date" value="1" defaultChecked={date === "1"} onChange={setDate}>Сегодня {humanizeDate(start1)} - {end1.toLocaleString("ru", { hour: "2-digit", minute: "2-digit" })}</Radio>
       <br />
-      <Radio name="date" value="2" defaultChecked={date === "2"} onChange={setDate}>Сегодня {humanizeDate(start2)} - {start2.getHours() + 2}:{start2.getMinutes()}</Radio>
+      <Radio name="date" value="2" defaultChecked={date === "2"} onChange={setDate}>Сегодня {humanizeDate(start2)} - {end2.toLocaleString("ru", { hour: "2-digit", minute: "2-digit" })}</Radio>
       <br />
-      <Radio name="date" value="3" defaultChecked={date === "3"} onChange={setDate}>Завтра {humanizeDate(start3)} - {start3.getHours() + 1}:{start3.getMinutes()}</Radio>
+      <Radio name="date" value="3" defaultChecked={date === "3"} onChange={setDate}>Завтра {humanizeDate(start3)} - {end3.toLocaleString("ru", { hour: "2-digit", minute: "2-digit" })}</Radio>
       <br />
-      <Radio name="date" value="4" defaultChecked={date === "4"} onChange={setDate}>Завтра {humanizeDate(start4)} - {start4.getHours() + 2}:{start4.getMinutes()}</Radio>
+      <Radio name="date" value="4" defaultChecked={date === "4"} onChange={setDate}>Завтра {humanizeDate(start4)} - {end4.toLocaleString("ru", { hour: "2-digit", minute: "2-digit" })}</Radio>
       <br />
       <br />
       <Row>
         <Input type="number" iconName="rub" placeholder="Введите  сумму..." defaultValue={price} onInput={inputValue(setPrice)}>Укажите  сумму  минимальной ставки</Input>
-        <InputAddress placeholder="Укажите город..." defaultValue={city} onInput={inputValue(setCity)}>Укажите ваш город</InputAddress>
+        <Input placeholder="Укажите город..." defaultValue={city} onInput={inputValue(setCity)}>Укажите ваш город</Input>
       </Row>
       <br />
       <br />
